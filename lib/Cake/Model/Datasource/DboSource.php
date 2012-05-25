36609099a67aa07145130da7cee530a2bb30dafe
Majna <majnaggz@gmail.com>
2012-05-26 01:06:16 +0200

    Fix issue when joined record is not found.

--- lib/Cake/Model/Datasource/DboSource.php
+++ lib/Cake/Model/Datasource/DboSource.php
@@ -1262,22 +1262,17 @@ class DboSource extends DataSource {
 			foreach ($resultSet as &$row) {
 				if ($type !== 'hasAndBelongsToMany') {
 					$q = $this->insertQueryData($query, $row, $association, $assocData, $model, $linkModel, $stack);
+					$fetch = null;
 					if ($q !== false) {
-						$reuseJoins = false;
+						$reuseData = array();
 						if (($type === 'belongsTo' || $type === 'hasOne') && isset($row[$linkModel->alias], $joined[$model->alias]) && in_array($linkModel->alias, $joined[$model->alias])) {
-							$reuseData = Hash::filter($row[$linkModel->alias]);
+							$reuseData = Set::filter($row[$linkModel->alias]);
 							if (!empty($reuseData)) {
-								$reuseJoins = true;
+								$fetch[0] = array($linkModel->alias => $row[$linkModel->alias]);
 							}
-						}
-
-						if ($reuseJoins) {	
-							$fetch[0] = array($linkModel->alias => $row[$linkModel->alias]);
 						} else {
 							$fetch = $this->fetchAll($q, $model->cacheQueries);
 						}
-					} else {
-						$fetch = null;
 					}
 				}
 				$selfJoin = $linkModel->name === $model->name;
